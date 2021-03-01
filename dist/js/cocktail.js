42975bc2b5c91fd98a1d3814ccabae1af129125a
David <dieavid01@gmail.com>
2021-03-01 01:52:45 +0100

    search working on recipe page

--- dist/js/cocktail.js
+++ dist/js/cocktail.js
@@ -1,23 +1,48 @@
 import COCKTAILDB_API_KEY from './apiKey.js';
 
 class Cocktails {
-    // fetch cocktail from api database
-    async getCocktails(cocktail) {
+    // Search API database
+    async searchCocktail(inputData) {
         try {
-            const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/search.php?s=${cocktail}`);
+            const response = await fetch(`https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/search.php?s=${inputData}`);
             const res = await response.json();
             const data = await res.drinks;
-            return { cocktail: data };
+            return data;
         } catch (error) {
             console.error(error.message);
         }
-
     };
 
-    async getPopularCocktails() {
-        const res = await fetch(`https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/popular.php`);
-        const resData = await res.json();
-        return resData.drinks;
+    // retrive data from API
+    async getCocktails(retrievalType) {
+        let endpoint;
+
+        // change endpoint depending on the type of information to retrieve
+        switch (retrievalType) {
+            case 'popular':
+                // endpoint = `https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/popular.php`;
+                break;
+            
+            case 'latest':
+                // endpoint = `https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/latest.php`;
+                break;
+
+            case 'random':
+                // endpoint = `https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/randomselection.php`;
+                break;
+
+            default:
+                // endpoint = `https://www.thecocktaildb.com/api/json/v2/${COCKTAILDB_API_KEY}/latest.php`;
+                break;
+        }
+
+        try {
+            const res = await fetch(endpoint);
+            const resData = await res.json();
+            return resData.drinks;
+        } catch (error) {
+            console.error("error at API", error.message);
+        }
     }
 }
 
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,12 +1,13 @@
-import {
-    cocktails
-} from './cocktail.js';
-
-
+import { cocktails } from './cocktail.js';
+import { ui } from './ui.js'
 
 const menuButton = document.querySelector('.menu-toggle');
+const searchInput = document.querySelector('#search');
+const recipes = document.querySelectorAll('.recipes');
+const close = document.querySelector('.close');
 
-const toggleMenu = () => {
+// functions
+function toggleMenu() {
     const headerNav = document.querySelector('.header-nav');
     const menuLinks = document.querySelectorAll('.menu-item');
 
@@ -15,134 +16,64 @@ const toggleMenu = () => {
     menuLinks.forEach(item => item.classList.toggle('open'));
 }
 
-menuButton.addEventListener('click', toggleMenu);
-
-const searchInput = document.querySelector('#search');
-
-searchInput.addEventListener('keyup', () => {
-
+function displaySearchedCocktails() {
     const content = document.querySelector('.content .recipes');
-    let output = '';
 
-    cocktails.getCocktails(searchInput.value)
+    cocktails.searchCocktail(searchInput.value)
         .then(drinks => {
             content.innerHTML = '';
             content.classList.remove('empty');
 
-            drinks.cocktail.forEach(drink => {
-                output = `
-           <figure class="recipe-item">
-              <img src="${drink.strDrinkThumb}" alt="${drink.strDrink} cocktail">
-              <figcaption>
-                  <p class="recipe-name">${drink.strDrink}</p>
-                  <p class="hide recipe-instructions">${drink.strInstructions}</p>
-                  <ul class="hide recipe-ingredients">
-                    <li>${drink.strIngredient1}</li>
-                    <li>${drink.strIngredient2}</li>
-                    <li>${drink.strIngredient3}</li>
-                    <li>${drink.strIngredient4}</li>
-                    <li>${drink.strIngredient5}</li>
-                    <li>${drink.strIngredient6}</li>
-                    <li>${drink.strIngredient7}</li>
-                    <li>${drink.strIngredient8}</li>
-                    <li>${drink.strIngredient9}</li>
-                    <li>${drink.strIngredient10}</li>
-                    <li>${drink.strIngredient11}</li>
-                    <li>${drink.strIngredient12}</li>
-                    <li>${drink.strIngredient13}</li>
-                    <li>${drink.strIngredient14}</li>
-                    <li>${drink.strIngredient15}</li>
-                  </ul>
-                  <a href="recipes.html" class="btn-secondary">View Recipe</a>
-              </figcaption>
-          </figure>`
-                content.innerHTML += output;
-            });
+            ui.displayCocktails(drinks, content);
         });
-})
-
-const modal = document.querySelector('.modal-wrapper');
-const recipes = document.querySelectorAll('.recipes');
-recipes.forEach(recipeSection => {
-    recipeSection.addEventListener('click', (e) => {
-        console.log(e.target.closest('figure'));
-
-        if (e.target.closest('figure')) {
-            const recipeItem = e.target.closest('figure');
-            modal.classList.add('open');
-            const recipeName = recipeItem.querySelector('.recipe-name');
-            const recipeImage = recipeItem.querySelector('img');
-            const recipeInstructions = recipeItem.querySelector('.recipe-instructions');
-            const recipeIngrdients = recipeItem.querySelectorAll('.recipe-ingredients li');
-
-            const name = modal.querySelector('.name');
-            const image = modal.querySelector('img');
-            const steps = modal.querySelector('.steps');
-            const ingredients = modal.querySelector('.ingredients');
-
-            name.innerHTML = recipeName.textContent;
-            steps.innerHTML = recipeInstructions.textContent;
-            image.setAttribute('src', recipeImage.getAttribute('src'));
-            console.log([...recipeIngrdients])
-            let output = '';
-            [...recipeIngrdients].forEach(ingredient => {
-                if (ingredient.textContent !== "null") {
-                    output += `<li>${ingredient.textContent}</li>`
-                    // console.log(ingredient)
-                }
-            })
-            console.log(output)
-            ingredients.innerHTML = output;
-        }
-    })
-})
-
-
-const close = document.querySelector('.close');
-close.addEventListener('click', closeModal);
-
-function closeModal() {
-    modal.classList.remove('open');
-    // modal.querySelector('.name').innerHTML = '';
-    // modal.querySelector('img').innerHTML = '';
-    // modal.querySelector('.steps').innerHTML = '';
-    // modal.querySelector('.ingredients').innerHTML = '';
 }
 
+function displaySectionCocktails() {
+    const randomSection = document.querySelector('.random .recipes');
+    const popularSection = document.querySelector('.popular .recipes');
+    const latestSection = document.querySelector('.latest .recipes');
 
-document.addEventListener("DOMContentLoaded", () => {
-    const popularSection = document.querySelector('.highlights .recipes')
-    let output = '';
-    cocktails.getPopularCocktails()
-        .then(data => {
-            data.forEach(item => {
-                output = `
-                <figure class="recipe-item">
-                   <img src="${item.strDrinkThumb}" alt="${item.strDrink} cocktail">
-                   <figcaption>
-                       <p class="recipe-name">${item.strDrink}</p>
-                       <p class="hide recipe-instructions">$itemk.strInstructions}</p>
-                       <ul class="hide recipe-ingredients">
-                         <li>${item.strIngredient1}</li>
-                         <li>${item.strIngredient2}</li>
-                         <li>${item.strIngredient3}</li>
-                         <li>${item.strIngredient4}</li>
-                         <li>${item.strIngredient5}</li>
-                         <li>${item.strIngredient6}</li>
-                         <li>${item.strIngredient7}</li>
-                         <li>${item.strIngredient8}</li>
-                         <li>${item.strIngredient9}</li>
-                         <li>${item.strIngredient10}</li>
-                         <li>${item.strIngredient11}</li>
-                         <li>${item.strIngredient12}</li>
-                         <li>${item.strIngredient13}</li>
-                         <li>${item.strIngredient14}</li>
-                         <li>${item.strIngredient15}</li>
-                       </ul>
-                       <a href="recipes.html" class="btn-secondary">View Recipe</a>
-                   </figcaption>
-               </figure>`
-                popularSection.innerHTML += output;
-            });
-        })
+    // display random cocktails if on the index page to avoid console error
+    if (randomSection) {
+        cocktails.getCocktails('random')
+            .then(data => {
+                ui.displayCocktails(data, randomSection)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    // display popular cocktails if on the main recipe page to avoid console error
+    if (popularSection) {
+        cocktails.getCocktails('popular')
+            .then(data => {
+                ui.displayCocktails(data, popularSection)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    // display latest cocktails if on the main recipe page
+    if (latestSection) {
+        cocktails.getCocktails('latest')
+            .then(data => {
+                ui.displayCocktails(data, latestSection)
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+}
+
+// event listeners
+menuButton.addEventListener('click', toggleMenu);
+searchInput.addEventListener('keyup', displaySearchedCocktails);
+document.addEventListener("DOMContentLoaded", displaySectionCocktails);
+recipes.forEach(recipeSection => {
+    recipeSection.addEventListener('click', (e) => {
+        ui.displayModal(e.target)
+    })
 })
+close.addEventListener('click', ui.closeModal);
