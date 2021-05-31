f095b3a194433c7972394c8702e9373ea241e394
David <dieavid01@gmail.com>
2021-05-31 01:07:04 +0100

    form validation on search

--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -11,6 +11,7 @@ const Recipes = () => {
     const inputRef = useRef();
     const [ searchWord, setSearchWord ] = useState('');
     const [ data, setData ] = useState([]);
+    const [ error, setError ] = useState("");
 
     useEffect(() => {
         inputRef.current.focus();
@@ -18,13 +19,28 @@ const Recipes = () => {
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const res = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWord}`);
-        
-        const resData = res.data.drinks;
 
-        setData(resData);
-
-        console.log(resData);
+        if (searchWord === "") {
+            document.querySelector('form small').classList.add(styles.error);
+            setError("");
+            return;
+        } else {
+            document.querySelector('form small').classList.remove(styles.error);
+            axios.get(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchWord}`)
+            .then(res => {
+                if (res.data.drinks === null) {
+                    setData(null);
+                    setError(<p>Could not find what you were looking for &#128531;. Please, try something else.</p>)
+                } else {
+                    setData(res.data.drinks);
+                    setError("");
+                }
+            })
+            .catch(err => {
+                setError(err.message);
+                setData(null);
+            })
+        }
     }
 
     return (
@@ -39,14 +55,16 @@ const Recipes = () => {
                         onSubmit={handleSubmit}    
                     >
                         <div className={styles.formGroup}>
-                            <label htmlFor="Search name">Search name</label>
-                            <input 
-                                ref={inputRef} 
-                                type="text"
-                                value={searchWord}
-                                onChange={(e) => setSearchWord(e.target.value)}    
-                            />
-                            <p>{searchWord}</p>
+                            <label >
+                                Search name
+                                <input 
+                                    ref={inputRef} 
+                                    type="text"
+                                    value={searchWord}
+                                    onChange={(e) => setSearchWord(e.target.value)}    
+                                />
+                                <small>Please search for something valid</small>
+                            </label>
                             <Button type="submit">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="17" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -59,7 +77,11 @@ const Recipes = () => {
                 <div className={styles.allRecipes}>
                     <h3>All <span>Recipes</span></h3>
 
-                    <RecipeContainer searchedData={data} />
+                    { error ? (
+                        <p className={styles.error}> {error} </p>
+                    ): (
+                        <RecipeContainer searchedData={data} />
+                    )}
                 </div>
             </div>
     )
