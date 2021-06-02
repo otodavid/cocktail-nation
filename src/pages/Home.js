eecba5ffad62600f9aa3b6edcae29b1796be6c02
David <dieavid01@gmail.com>
2021-06-02 21:45:31 +0100

    added custom logo and updated title for each page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,62 +1,70 @@
-import image from '../assets/hero-image.png';
-import { Button } from '../components/Button';
-import { SlantContainer } from '../components/SlantContainer';
-import { ToolsContainer } from '../components/ToolsContainer';
-import { RecipeContainer } from '../components/RecipeContainer';
-import { GradientBackground } from '../components/GradientBackground';
-import './Home.scss';
-import pic from '../assets/banner.jpg';
-import { Link } from 'react-router-dom';
+import image from "../assets/hero-image.png";
+import { Button } from "../components/Button";
+import { SlantContainer } from "../components/SlantContainer";
+import { ToolsContainer } from "../components/ToolsContainer";
+import { RecipeContainer } from "../components/RecipeContainer";
+import { GradientBackground } from "../components/GradientBackground";
+import "./Home.scss";
+import pic from "../assets/banner.jpg";
+import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 const Home = () => {
-    // const modal = useContext(ModalContext);
-    // console.log(modal);
+  return (
+    <>
+    <Helmet>
+        <title>Cocktail Nation</title>
+    </Helmet>
+      <div className="home">
+        <section className="hero">
+          <div className="hero-content">
+            <h1>
+              homemade <span>coctails</span> can be better
+            </h1>
+            <p>
+              Why go out to get a couple of drinks when you can make your own
+              elegant cocktails at home? You ready?
+            </p>
+            <Link to="/recipes">
+              <Button className="btn">view recipes</Button>
+            </Link>
+          </div>
+          <div className="hero-image">
+            <img src={image} alt="Red cocktail in a glass cup" />
+          </div>
+          <SlantContainer pos="-1px" />
+        </section>
 
-    return (
-        <div className="home">
-            <section className="hero">
-                <div className="hero-content">
-                    <h1>homemade <span>coctails</span> can be better</h1>
-                    <p>
-                        Why go out to get a couple of drinks when you can make your own elegant cocktails at home? You ready?
-                    </p>
-                    <Link to="/recipes">
-                        <Button className="btn">view recipes</Button>
-                    </Link>
-                </div>
-                <div className="hero-image">
-                    <img src={image} alt="Red cocktail in a glass cup"/>
-                </div>
-                <SlantContainer pos="-1px" />
-            </section>
+        <section className="tools-highlight">
+          <h2>
+            Get the right <span>tools</span>
+          </h2>
+          <ToolsContainer numberOfTools={3} />
+          <Link to="/tools">
+            <Button className="btn">view all</Button>
+          </Link>
 
-            <section className="tools-highlight">
-                <h2>Get the right <span>tools</span></h2>
-                <ToolsContainer numberOfTools={3} />
-                <Link to="/tools">
-                    <Button className="btn">
-                        view all
-                    </Button>
-                </Link>
+          <SlantContainer bgColor="#303134" pos="-1px" />
+        </section>
 
-                <SlantContainer bgColor="#303134" pos="-1px" />
-            </section>
+        <section className="top-recipes">
+          <h2>
+            Top <span>recipes</span>
+          </h2>
+          <RecipeContainer />
+          <Link to="/recipes">
+            <Button className="btn">view more</Button>
+          </Link>
+        </section>
 
-            <section className="top-recipes">
-                <h2>Top <span>recipes</span></h2>
-                <RecipeContainer />
-                <Link to="/recipes">
-                    <Button className="btn">
-                        view more
-                    </Button>
-                </Link>
-            </section>
+        <GradientBackground bgImage={pic}>
+          <h1>
+            Try a deliciously elegant <span>cocktail</span> now
+          </h1>
+        </GradientBackground>
+      </div>
+    </>
+  );
+};
 
-            <GradientBackground bgImage={pic}>
-                <h1>Try a deliciously elegant <span>cocktail</span> now</h1>
-            </GradientBackground>
-        </div>
-    )
-}
-
-export default Home
+export default Home;
--- src/pages/Recipes.js
+++ src/pages/Recipes.js
@@ -6,6 +6,7 @@ import styles from "./Recipes.module.scss";
 import { useEffect, useRef, useState } from "react";
 import axios from "axios";
 import { useForm } from "react-hook-form";
+import { Helmet } from "react-helmet";
 
 const Recipes = () => {
   const inputRef = useRef(null);
@@ -48,59 +49,64 @@ const Recipes = () => {
   };
 
   return (
-    <div className={styles.recipes}>
-      <GradientBackground bgImage={recipePic}>
-        <h2>
-          Search <span>Cocktails</span>
-        </h2>
-        <p>
-          Search for your favorite recipe or try something new with our
-          up-to-date recipes
-        </p>
-        <form className={styles.form} onSubmit={handleSubmit(handleSearch)}>
-          <div className={styles.formGroup}>
-            <label htmlFor="search">Search name</label>
-            <input
-              id="search"
-              type="text"
-              {...register("search", {
-                required: "Please search for something",
-              })}
-              ref={inputRef}
-            />
-            <small>{errors.search?.message}</small>
-            <Button type="submit">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="17"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                />
-              </svg>
-            </Button>
-          </div>
-        </form>
-      </GradientBackground>
+    <>
+      <Helmet>
+        <title>Cocktail Nation | Recipes</title>
+      </Helmet>
+      <div className={styles.recipes}>
+        <GradientBackground bgImage={recipePic}>
+          <h2>
+            Search <span>Cocktails</span>
+          </h2>
+          <p>
+            Search for your favorite recipe or try something new with our
+            up-to-date recipes
+          </p>
+          <form className={styles.form} onSubmit={handleSubmit(handleSearch)}>
+            <div className={styles.formGroup}>
+              <label htmlFor="search">Search name</label>
+              <input
+                id="search"
+                type="text"
+                {...register("search", {
+                  required: "Please search for something",
+                })}
+                ref={inputRef}
+              />
+              <small>{errors.search?.message}</small>
+              <Button type="submit">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="17"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth={2}
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                  />
+                </svg>
+              </Button>
+            </div>
+          </form>
+        </GradientBackground>
 
-      <div className={styles.allRecipes}>
-        <h3>
-          All <span>Recipes</span>
-        </h3>
+        <div className={styles.allRecipes}>
+          <h3>
+            All <span>Recipes</span>
+          </h3>
 
-        {error ? (
-          <p className={styles.error}> {error} </p>
-        ) : (
-          <RecipeContainer searchedData={data} />
-        )}
+          {error ? (
+            <p className={styles.error}> {error} </p>
+          ) : (
+            <RecipeContainer searchedData={data} />
+          )}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
